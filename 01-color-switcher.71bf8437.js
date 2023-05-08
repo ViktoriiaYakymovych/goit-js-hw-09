aac2ef44f68132489939002b76daa739e658f7d3
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-05-08 09:24:33 +0000

    Deploying to gh-pages from @ ViktoriiaYakymovych/goit-js-hw-09@6ff31cceea20236131c4c1945a4784fb8385c93d 🚀

---
+++ 01-color-switcher.71bf8437.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")};let e=0;t.startBtn.addEventListener("click",(()=>{e=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),isIntervalActive=!0,t.startBtn.disabled=!0})),t.stopBtn.addEventListener("click",(function(){clearInterval(e),isIntervalActive=!1,t.startBtn.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.71bf8437.js.map
