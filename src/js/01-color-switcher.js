b8cb15ac3a1c6582d16878d796a1a17c24d80ef1
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-05-11 08:54:11 +0300

    fixed task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,22 +5,17 @@ const refs = {
 
 const TIME_UPDATE_COLOR = 1000;
 let intervalId = 0;
-let intervalActive = false;
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
 
-refs.startBtn.addEventListener('click', () => {
-    if (intervalActive) return;
-    else onChangeColor();
-})
+refs.startBtn.addEventListener('click', onChangeColor)
 
 function onChangeColor () {
     intervalId = setInterval(() => {
         document.body.style.backgroundColor = getRandomHexColor();
     }, TIME_UPDATE_COLOR);
-    isIntervalActive = true;
     refs.startBtn.disabled = true;
 };
 
@@ -28,7 +23,6 @@ refs.stopBtn.addEventListener('click', onDeleteColor);
 
 function onDeleteColor () {
     clearInterval(intervalId);
-    isIntervalActive = false;
     refs.startBtn.disabled = false;
 };
 
